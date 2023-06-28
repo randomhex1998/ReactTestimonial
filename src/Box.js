39e494078ea5668953100b4d1ef4a4d3db6d423a
randomhex1998 <randomhex1998@gmail.com>
2023-06-28 15:11:38 +0430

    add box styles

--- src/Box.js
+++ src/Box.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import RayanProfile from "./img/rayan.jpg";
 
 const Box = () => {
     return (
-        <div className='box area'>
+        <div className='box-area'>
             <div className='box'>
-                <div className='img-area'><img src="" alt="" /></div>
+                <div className='img-area'><img src={RayanProfile} alt="" /></div>
                 <h3 className='title'>رایان جابری</h3>
                 <p className='para'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و بارم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با ، و</p>
             </div>
