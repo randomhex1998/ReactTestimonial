669637db9f5d11237712be5371d84e0e4bf836bc
randomhex1998 <randomhex1998@gmail.com>
2023-06-28 15:23:26 +0430

    make data files

--- src/Box.js
+++ src/Box.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import RayanProfile from "./img/rayan.jpg";
+import {FaChevronLeft , FaChevronRight} from "react-icons/fa"
 
 const Box = () => {
     return (
@@ -10,8 +11,8 @@ const Box = () => {
                 <p className='para'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و بارم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با ، و</p>
             </div>
             <div className='btns-box'>
-                <button className='btn btn-right'>+</button>
-                <button className='btn btn-left'>-</button>
+                <button className='btn btn-right'><FaChevronRight/></button>
+                <button className='btn btn-left'><FaChevronLeft/></button>
             </div>
         </div>
     );
