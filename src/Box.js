5c13917217bf802fc6d34aed1592852f8216c520
randomhex1998 <randomhex1998@gmail.com>
2023-06-28 15:00:58 +0430

    make box items in jsx

--- src/Box.js
+++ src/Box.js
@@ -5,8 +5,8 @@ const Box = () => {
         <div className='box area'>
             <div className='box'>
                 <div className='img-area'><img src="" alt="" /></div>
-                <h3 className='title'></h3>
-                <p></p>
+                <h3 className='title'>رایان جابری</h3>
+                <p className='para'>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و بارم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با ، و</p>
             </div>
         </div>
     );
